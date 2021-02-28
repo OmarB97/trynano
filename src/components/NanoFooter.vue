30fe6cca0f9aa23ea42e8705bed22129480f9800
Omar Baradei <omarbaradei21@gmail.com>
2021-02-27 16:55:26 -0800

    moved feedback form out of the footer component into its own component

--- src/components/NanoFooter.vue
+++ src/components/NanoFooter.vue
@@ -29,151 +29,41 @@
       </span>
       <span class="footer-content">
         Give feedback
-        <a class="feedback-link" @click="feedbackDialogVisible = true">here</a>.
+        <a class="feedback-link" @click="toggleFeedbackDialogVisibility(true)">here</a>.
       </span>
-      <el-dialog title="Give Feedback" v-model="feedbackDialogVisible" width="40%">
-        <el-form
-          :model="ruleForm"
-          class="form"
-          ref="form"
-          :rules="rules"
-          status-icon
-          label-width="120px"
-        >
-          <el-form-item label="Name" prop="name">
-            <el-input v-model="name" name="name"></el-input>
-          </el-form-item>
-          <el-form-item label="Email" prop="email">
-            <el-input v-model="email" name="email"></el-input>
-          </el-form-item>
-          <el-form-item label="Feedback Type">
-            <el-radio-group v-model="feedbackType" name="type">
-              <el-radio label="Bug"></el-radio>
-              <el-radio label="Feature Request"></el-radio>
-              <el-radio label="Other"></el-radio>
-            </el-radio-group>
-          </el-form-item>
-          <el-form-item label="Message" prop="message">
-            <el-input
-              type="textarea"
-              v-model="message"
-              name="message"
-              maxlength="500"
-              show-word-limit
-            ></el-input>
-          </el-form-item>
-        </el-form>
-        <template #footer>
-          <span class="dialog-footer">
-            <el-button @click="feedbackDialogVisible = false">Cancel</el-button>
-            <el-button
-              :disabled="name === '' || email === '' || message === ''"
-              type="primary"
-              @click="onSubmitFeedbackForm"
-              >Confirm</el-button
-            >
-          </span>
-        </template>
-      </el-dialog>
+      <FeedbackForm
+        :feedbackDialogVisible="feedbackDialogVisible"
+        @hideFeedbackForm="toggleFeedbackDialogVisibility(false)"
+      ></FeedbackForm>
     </div>
   </footer>
 </template>
 
 <script>
-import { ref } from 'vue';
-import emailjs from 'emailjs-com';
-import { ElMessage } from 'element-plus';
+import { ref, getCurrentInstance } from 'vue';
+import FeedbackForm from './FeedbackForm.vue';
 
 export default {
   name: 'NanoFooter',
+  components: {
+    FeedbackForm,
+  },
   setup() {
+    const { emitter } = getCurrentInstance().appContext.config.globalProperties;
+
     const feedbackDialogVisible = ref(false);
 
-    const form = ref(null);
-
-    const name = ref('');
-    const email = ref('');
-    const feedbackType = ref('Bug');
-    const message = ref('');
-
-    const ruleForm = ref({
-      name,
-      email,
-      feedbackType,
-      message,
-    });
-
-    const rules = ref({
-      name: [{ required: true, message: 'Please enter a name', trigger: 'blur' }],
-      email: [
-        { required: true, message: 'Please enter an email', trigger: 'blur' },
-        { type: 'email', message: 'Invalid email address', trigger: 'blur' },
-      ],
-      message: [
-        { required: true, message: 'Please enter a message', trigger: 'blur' },
-        { min: 10, message: 'Please enter more than 10 characters', trigger: 'blur' },
-      ],
-    });
-
-    const onSubmitFeedbackForm = () => {
-      feedbackDialogVisible.value = false;
-      try {
-        form.value.validate((isValid) => {
-          if (!isValid) {
-            ElMessage({
-              message: `Error submitting feedback, form was invalid`,
-              type: 'error',
-            });
-            return;
-          }
-          emailjs
-            .sendForm(
-              process.env.VUE_APP_EMAILJS_SERVICE_KEY,
-              process.env.VUE_APP_EMAILJS_TEMPLATE_KEY,
-              '.form',
-              process.env.VUE_APP_EMAILJS_USER_KEY
-            )
-            .then((response) => {
-              console.log(response);
-              if (response && response.status === 200) {
-                ElMessage({
-                  message: 'Feedback successfully sent!',
-                  type: 'success',
-                });
-
-                // Reset form field
-                name.value = '';
-                email.value = '';
-                feedbackType.value = 'Bug';
-                message.value = '';
-              } else {
-                ElMessage({
-                  message: `Error submitting feedback${
-                    response.error !== null ? `: ${response.error}` : ''
-                  }`,
-                  type: 'error',
-                });
-              }
-            });
-        });
-      } catch (error) {
-        ElMessage({
-          message: `Error submitting feedback: ${error}`,
-          type: 'error',
-        });
-      }
+    const toggleFeedbackDialogVisibility = (isVisible) => {
+      emitter.emit('toggle-feedback-form-visibility', isVisible);
     };
+
+    emitter.on('toggle-feedback-form-visibility', (isVisible) => {
+      feedbackDialogVisible.value = isVisible;
+    });
 
     return {
       feedbackDialogVisible,
-      onSubmitFeedbackForm,
-      form,
-      ruleForm,
-      rules,
-      name,
-      email,
-      feedbackType,
-      message,
+      toggleFeedbackDialogVisibility,
     };
   },
 };
