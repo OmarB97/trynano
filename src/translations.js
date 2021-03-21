40a7f7504ae34c8c570e0ec177ebb4c8964e99de
Omar Baradei <omarbaradei21@gmail.com>
2021-03-21 00:18:26 -0700

    translated NanoDemo.vue, also start tracking the new translation folders/files

---
+++ src/translations.js
@@ -0,0 +1,13 @@
+import en from './locales/en.json';
+
+/* To add a new translation: 
+    1. create a new json file under ./locals/<language>.json
+    2. Follow the same structure seen in the other translation files
+    3. import it inside this file
+    4. Add it to the list below
+*/
+const translations = {
+    en, /* english */
+}
+
+export default translations;
